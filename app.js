c4d6cc0e8edbcb4d03105f92a5d4ead16c4b363c
mattreyes7 <mattreyes615@gmail.com>
2017-01-31 20:18:08 -0800

    Rewrote questions 6-8; added score tally

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var userName = prompt('What\'s your name?').toLowerCase();
+// ask user name
 alert('Hello'+' '+ userName + '. Thanks for coming to my site! First, you need to answer a short survey.');
+// this is the question counter
+var questionCounter = 0;
 
 // We want to ask the user a question
 // Take their answer as input stored in a variable
@@ -43,38 +46,40 @@ if (answerFour === myAnswerFour) {
 }
 
 var answerFive = prompt('Do you like to travel?').toLowerCase();
-var myAnswerFive = ['yes'];
+var myAnswerFive = ['y', 'yes'];
 
 // checks to see if user answer matches above
 // an appropriate alert will run based on answer.
 
-if ((answerFive === myAnswerFive) || (myAnswerFive !== answerFive)) {
+if (myAnswerFive.indexOf(answerFive) > -1) {
   alert('Awesome! I\'ve been to at least 3 countries.');
 } else {
   alert('Get out there and see the world!');
 }
-
+//question 6
 var answerSix = prompt('Let\'s make this a little harder. I have a ton of favorite movies. What is my favorite one?');
 var myAnswerSix = [ 'The Lord of the Rings Trilogy', 'Star Wars trilogy', 'Guardians of the Galaxy' ];
 
-if (answerSix === myAnswerSix.indexOf(-1)) {
-  alert('I think I need to add that to my watchlist!');
+if (myAnswerSix.indexOf(answerSix) > -1) {
+  alert('You got it!');
+  questionCounter++;
 } else {
-  alert('You got it!')
+  alert('I need to add that to my watchlist!')
 }
 // As a developer, I want to add a seventh question to my guessing game that takes numeric input, and indicates to the user whether the guess is 'too high' or 'too low', and gives the user exactly four opportunities to get the correct answer, so that my fancy programming skills are showcased.
 // code runs a while loop that counts user input on prompt for up to 4 times. when user guesses the correct answer the loop stops. the loops stops stop after 4 tries.
 
 var myAnswerSeven = 7;
-var counter = 1;
+var counterSeven = 0;
 
-while (counter < 5) {
-  counter++;
+while (counterSeven < 4) {
+  counterSeven++;
   var answerSeven = parseInt(prompt('What\'s my favorite number?'));
-if (counter === 5) {
+if (counterSeven === 5) {
   alert('Let\s move on');
 } else if (answerSeven === myAnswerSeven) {
   alert('Good guess!');
+  questionCounter++;
   break;
 } else if (answerSeven < myAnswerSeven) {
   alert('A little higher!');
@@ -85,16 +90,18 @@ if (counter === 5) {
 // As a developer, I want to add a question to my guessing game that accepts multiple possible correct answers that are stored in an array, for instance, "Can you guess a state that I have lived in besides Washington?", so that even more of my programming skills are showcased. For now, I will structure this question so that the user has six tries to get a single correct answer, and upon exhausting those tries OR getting a correct answer, displays a message to the user indicating all of the possible correct answers.
 
 var myAnswerEight = ['baseball', 'basketball', 'table tennis', 'golf'];
+var counterEight = 1;
 
-while (counter < 7) {
-  counter++;
+while (counterEight < 7) {
+  counterEight++;
   var answerEight = prompt('What is my favorite sport?');
-if (counter === 7) {
+if (counterEight === 7) {
     alert('I like baseball, basketball, table tennis, and golf!');
-  } else if (answerEight === myAnswerEight[0] || answerEight === myAnswerEight[1] || answerEight === myAnswerEight[2] || answerEight === myAnswerEight[3]) {
+  } else if (myAnswerEight.indexOf(answerEight) > -1) {
     alert('That is correct!');
+    questionCounter++;
     break;
   }
 }
 
-alert('Thanks for playing' +' '+ userName);
+alert('Thanks for playing' +' '+ userName + '. You got '+ questionCounter + ' correct answers!');
