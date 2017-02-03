a0aab6f4f62c6d495b4afcabd27d00d2e30aabd4
Chase Cross <chaseacross@gmail.com>
2017-02-02 20:55:26 -0800

    Commented out code to make questions more discrete, specificy which required elements were present

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 var questionCounter = 0;
 
 var userName = prompt('What\'s your name?').toLowerCase();
-// game prompts user name
+// game prompts user name//
 alert('Hello'+' '+ userName + '. Thanks for coming to my site! First, you need to answer a short survey.');
 // this is the question counter below.
 console.log(userName);
 
-
+//Question one//
 function questionOne() {
   var answerOne = prompt('Are you ready to begin? yes or no.').toLowerCase();
   var myAnswerOne = 'yes';
@@ -22,6 +22,7 @@ function questionOne() {
 
 questionOne();
 
+//Question two//
 function questionTwo() {
   var answerTwo = prompt('Do you play sports?').toLowerCase();
   var myAnswerTwo = 'yes';
@@ -36,6 +37,7 @@ function questionTwo() {
 
 questionTwo();
 
+//Question three//
 function questionThree() {
   var answerThree = prompt('Do you know how to code?').toLowerCase();
   var myAnswerThree = 'yes';
@@ -50,6 +52,7 @@ function questionThree() {
 
 questionThree();
 
+//Question four//
 function questionFour() {
   var answerFour = prompt('Do you play video games?').toLowerCase();
   var myAnswerFour = 'yes';
@@ -64,6 +67,7 @@ function questionFour() {
 
 questionFour();
 
+//Question five//
 function questionFive() {
   var answerFive = prompt('Do you like to travel?').toLowerCase();
   var myAnswerFive = ['y', 'yes'];
@@ -78,10 +82,11 @@ function questionFive() {
 
 questionFive();
 
+//Question six includes array//
 function questionSix() {
   var answerSix = prompt('Let\'s make this a little harder. I have a ton of favorite movies. Name at least one');
   var myAnswerSix = [ 'Mad Max', 'Inception', 'Guardians of the Galaxy' ];
-
+//Beginning of logic//
   if (myAnswerSix.indexOf(answerSix) > -1) {
     alert('You got it!');
     questionCounter++;
@@ -93,10 +98,11 @@ function questionSix() {
 
 questionSix();
 
+//Question seven includes feedback on whether answer is too high/too low//
 function questionSeven() {
   var myAnswerSeven = 7;
   var counterSeven = 0;
-
+//Beginning of logic//
   while (counterSeven < 4) {
     counterSeven++;
     var answerSeven = parseInt(prompt('What\'s my favorite number?'));
@@ -116,11 +122,11 @@ function questionSeven() {
 }
 
 questionSeven();
-
+//Question eight -- includes array and limits questions//
 function questionEight() {
   var myAnswerEight = ['baseball', 'basketball', 'table tennis', 'golf'];
   var counterEight = 1;
-
+//Beginning of logic//
   while (counterEight < 7) {
     counterEight++;
     var answerEight = prompt('Name one of my favorite sports.');
@@ -134,6 +140,6 @@ function questionEight() {
   }
   console.log(answerEight);
 }
-
+//Question counter only reflects scores for last three questions -- other questions do not have right or wrong answers//
 questionEight();
 alert('Thanks for playing' +' '+ userName + '. You got '+ questionCounter + ' correct answers out of 3!');
